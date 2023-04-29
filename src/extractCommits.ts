6f4074d6183b07c59add0f684bb70955190e7460
Julian Psotta <julianpsotta@gmail.com>
2023-04-29 11:03:40 +0200

    Add more checks and a more complete build setup.

--- src/extractCommits.ts
+++ src/extractCommits.ts
@@ -1,42 +1,46 @@
-import get from "lodash.get";
-import got from "got";
+import get from 'lodash.get'
+import got from 'got'
 
 type Commit = {
-    message: string;
-};
+  message: string
+}
 
+// @ts-ignore
 const extractCommits = async (context, core): Promise<Commit[]> => {
-    // For "push" events, commits can be found in the "context.payload.commits".
-    const pushCommits = Array.isArray(get(context, "payload.commits"));
-    if (pushCommits) {
-        return context.payload.commits;
+  // For "push" events, commits can be found in the "context.payload.commits".
+  const pushCommits = Array.isArray(get(context, 'payload.commits'))
+  if (pushCommits) {
+    return context.payload.commits
+  }
+
+  // For PRs, we need to get a list of commits via the GH API:
+  const prCommitsUrl = get(context, 'payload.pull_request.commits_url')
+  if (prCommitsUrl) {
+    try {
+      let requestHeaders = {
+        Accept: 'application/vnd.github+json'
+      }
+      if (core.getInput('GITHUB_TOKEN') != '') {
+        core.info('✅ Found GITHUB_TOKEN')
+        // @ts-ignore
+        requestHeaders['Authorization'] =
+          'token ' + core.getInput('GITHUB_TOKEN')
+      }
+      const {body} = await got.get(prCommitsUrl, {
+        responseType: 'json',
+        headers: requestHeaders
+      })
+
+      if (Array.isArray(body)) {
+        return body.map(item => item.commit)
+      }
+      return []
+    } catch {
+      return []
     }
+  }
 
-    // For PRs, we need to get a list of commits via the GH API:
-    const prCommitsUrl = get(context, "payload.pull_request.commits_url");
-    if (prCommitsUrl) {
-        try {
-            let requestHeaders = {
-                "Accept": "application/vnd.github+json",
-            }
-            if (core.getInput('GITHUB_TOKEN') != "") {
-                requestHeaders["Authorization"] = "token " + core.getInput('GITHUB_TOKEN')
-            }
-            const { body } = await got.get(prCommitsUrl, {
-                responseType: "json",
-                headers: requestHeaders,
-            });
+  return []
+}
 
-            if (Array.isArray(body)) {
-                return body.map((item) => item.commit);
-            }
-            return [];
-        } catch {
-            return [];
-        }
-    }
-
-    return [];
-};
-
-export default extractCommits;
+export default extractCommits
